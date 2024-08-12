5d3510be1a238818154c9c7aeb7faa491e531b40
rodrigo <lenilsonrodrigo123@gmail.com>
2024-08-12 11:41:19 -0300

    concluindo a entidade aula

--- src/main/java/com/API/API_CT/domain/model/Aula.java
+++ src/main/java/com/API/API_CT/domain/model/Aula.java
@@ -1,5 +1,50 @@
 package com.API.API_CT.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name = "tb_aula")
 public class Aula {
     
-}
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private Modalidade modalidade;
+    private String horario;
+    @ManyToOne
+    private Instrutor instrutor;
+    
+    //getters and setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Modalidade getModalidade() {
+        return modalidade;
+    }
+    public void setModalidade(Modalidade modalidade) {
+        this.modalidade = modalidade;
+    }
+    public String getHorario() {
+        return horario;
+    }
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+    public Instrutor getInstrutor() {
+        return instrutor;
+    }
+    public void setInstrutor(Instrutor instrutor) {
+        this.instrutor = instrutor;
+    }
+
+}    
+
