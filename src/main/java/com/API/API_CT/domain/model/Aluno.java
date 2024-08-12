98697fab1c9d66793578461cbcedc37428d61367
rodrigo <lenilsonrodrigo123@gmail.com>
2024-08-12 11:32:11 -0300

    Concluindo a Entidade Aluno

--- src/main/java/com/API/API_CT/domain/model/Aluno.java
+++ src/main/java/com/API/API_CT/domain/model/Aluno.java
@@ -1,5 +1,46 @@
 package com.API.API_CT.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_aluno")
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String faixa;
+    private String telefone;
+   
+   //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getFaixa() {
+        return faixa;
+    }
+    public void setFaixa(String faixa) {
+        this.faixa = faixa;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+
     
 }
