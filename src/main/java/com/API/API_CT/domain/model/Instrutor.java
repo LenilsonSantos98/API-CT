20fe419615632716332b1e256eef8b4e3d0437aa
rodrigo <lenilsonrodrigo123@gmail.com>
2024-08-13 10:33:52 -0300

    concluindo as entidades intrutor e presença

--- src/main/java/com/API/API_CT/domain/model/Instrutor.java
+++ src/main/java/com/API/API_CT/domain/model/Instrutor.java
@@ -1,5 +1,33 @@
 package com.API.API_CT.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_instrutor")
 public class Instrutor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String especialidade;
+    
+    // getters and setters 
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEspecialidade() {
+        return especialidade;
+    }
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
     
 }
--- src/main/java/com/API/API_CT/domain/model/Presenca.java
+++ src/main/java/com/API/API_CT/domain/model/Presenca.java
@@ -1,5 +1,49 @@
 package com.API.API_CT.domain.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name = "tb_presenca")
 public class Presenca {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Aluno aluno;
+    @ManyToOne
+    private Aula aula;
+    private LocalDate data;
+    
+    //getters and setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Aluno getAluno() {
+        return aluno;
+    }
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+    public Aula getAula() {
+        return aula;
+    }
+    public void setAula(Aula aula) {
+        this.aula = aula;
+    }
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
     
 }
